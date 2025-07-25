d5d6de038827f0c1fc0e4da2e6b6c8f27376f8d5
Alaina Patel <alainaapatel@gmail.com>
2025-07-25 13:38:32 -0700

    Interation 0 Done

--- toDo/ContentView.swift
+++ toDo/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showNewTask = false
     var body: some View {
         VStack{
             HStack{
@@ -16,7 +17,9 @@ struct ContentView: View {
                     .fontWeight(.black)
                 Spacer()
                 Button{
-                    
+                    withAnimation{
+                        showNewTask = true
+                    }
                 }
                 label: {
                     Text("+")
@@ -27,6 +30,9 @@ struct ContentView: View {
             }
             .padding()
             Spacer()
+        }
+        if showNewTask{
+            NewToDoView()
         }
         
     }
