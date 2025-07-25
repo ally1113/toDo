61b6e2741e829affb6cfd4f3c4f7d26a93653f58
Alaina Patel <alainaapatel@gmail.com>
2025-07-25 13:03:12 -0700

    Initial Commit

---
+++ toDo/toDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  toDoApp.swift
+//  toDo
+//
+//  Created by Scholar on 7/25/25.
+//
+
+import SwiftUI
+
+@main
+struct toDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ toDoTests/toDoTests.swift
@@ -0,0 +1,17 @@
+//
+//  toDoTests.swift
+//  toDoTests
+//
+//  Created by Scholar on 7/25/25.
+//
+
+import Testing
+@testable import toDo
+
+struct toDoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
